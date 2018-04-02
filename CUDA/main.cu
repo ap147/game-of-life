d6cfef5ac014fe1cc07e9ae60c1d00dde859ff1f
ap147 <amarjotparmar098@gmail.com>
2018-04-02 18:13:02 +1200

    Logic implmented, needs to be tested

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -3,8 +3,409 @@
 #include <math.h>
 #include <stdio.h>
 
+__device__
+int getIndex(int x, int y, int rows){
+	// (size * 3) * y + (x * 3)
+	int result;
+	result = (rows * 3) * y;
+	result = result + (x * 3);
+	return result;
+}
 
+__device__
+void printBoardd(int *gen ,int amountofCells, int rows){
+
+	int count = 0;
+	int rowCount = 0;
+	printf("\n");
+	while(count < amountofCells){
+		count++;
+		count++;
+		if(gen[count] == 0){
+			printf(" . ");
+		}else{
+			printf(" x ");
+		}
+		rowCount++;
+
+		if(rowCount == rows){
+			printf("\n");
+			rowCount = 0;
+		}
+		count++;
+	}
+}
+__device__
+int checkLeft(int index, int *gen, int rows)
+{
+	int xNeighbour;
+	int yNeighbour;
+	int x = gen[index];
+	int y = gen[index + 1];
+
+	// 1
+	if (gen[index] == 0)
+	{
+		xNeighbour = getIndex(rows - 1, y, rows);
+	} // 2
+	else
+	{
+		xNeighbour = index - 3;
+	}
+
+	if (gen[xNeighbour + 2] == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkRight(int index, int *gen, int rows)
+{
+	int xNeighbour;
+	// 1
+	if (gen[index] == rows -1)
+	{
+		xNeighbour = 0;
+	} // 2
+	else
+	{
+		xNeighbour = index + 3;
+	}
+
+	if (gen[xNeighbour + 2] == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkTop(int index, int *gen, int rows, int columns)
+{
+	int yNeighbour;
+	// 1
+	if (gen[index+1] == 0)
+	{
+		yNeighbour = getIndex(gen[index], columns - 1 , rows);
+	} // 2
+	else
+	{
+		yNeighbour = getIndex(gen[index], (gen[index + 1]- 1), rows);
+	}
+
+	if (gen[yNeighbour + 2] == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkBottom(int index, int *gen, int rows, int columns)
+{
+	int yNeighbour;
+
+	// 1
+	if (gen[index+1] == columns - 1)
+	{
+		yNeighbour = getIndex(gen[index], 0 , rows);
+	} // 2
+	else
+	{
+		yNeighbour = getIndex(gen[index], (gen[index + 1] + 1), rows);
+	}
+	
+	if (gen[yNeighbour + 2] == 1) 
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkDiagonalTL(int index, int *gen, int rows, int columns)
+{
+	int neighbourIndex;
+	int x = gen[index]; 
+	int y = gen[index + 1];
+	int xNeighbour; 
+	int yNeighbour;
+
+	// 1
+	if (x == 0 && y == 0)
+	{
+		xNeighbour = rows - 1;
+		yNeighbour = columns - 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 2
+	else if (y == 0)
+	{
+		xNeighbour = x - 1;
+		yNeighbour = columns - 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 3
+	else if (x == 0)
+	{
+		neighbourIndex = index - 3;//getIndex((rows - 1), gen[index - 2));
+	}
+	// 4
+	else
+	{
+		xNeighbour = x - 1;
+		yNeighbour = y - 1;
+
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+
+	if (gen[neighbourIndex + 2] == 1) //cells[y][xNeighbour].alive == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkDiagonalTR(int index, int *gen, int rows, int columns)
+{
+	int neighbourIndex;
+	int x = gen[index]; 
+	int y = gen[index + 1];
+	int xNeighbour; 
+	int yNeighbour;
+	// 1
+	if (x == (rows - 1) && y == 0)
+	{
+		xNeighbour = 0;
+		yNeighbour = columns - 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 2
+	else if (y == 0)
+	{
+		xNeighbour = x + 1;
+		yNeighbour = columns - 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 3
+	else if (x == (rows - 1))
+	{
+		xNeighbour = 0;
+		yNeighbour = y - 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 4
+	else
+	{
+		xNeighbour = x + 1;
+		yNeighbour = y -1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+
+	if (gen[neighbourIndex + 2] == 1) //cells[y][xNeighbour].alive == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkDiagonalBL(int index, int *gen, int rows, int columns)
+{
+	int neighbourIndex;
+	int x = gen[index]; 
+	int y = gen[index + 1];
+	int xNeighbour; 
+	int yNeighbour;
+	// 1
+	if (x == 0 && y == (columns -1))
+	{
+		xNeighbour = rows - 1;
+		yNeighbour = 0;
+		neighbourIndex = getIndex(x, y, rows);
+	}
+	// 2
+	else if (y == (columns - 1))
+	{
+		xNeighbour = x - 1;
+		yNeighbour = 0;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 3
+	else if (x == 0)
+	{
+		xNeighbour = rows - 1;
+		yNeighbour = y + 1;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 4
+	else
+	{
+		neighbourIndex = getIndex(x - 1, y + 1, rows);
+	}
+
+	if (gen[neighbourIndex + 2] == 1) 
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int checkDiagonalBR(int index, int *gen, int rows, int columns)
+{
+	int neighbourIndex;
+	int x = gen[index]; 
+	int y = gen[index + 1];
+	int xNeighbour; 
+	int yNeighbour;
+	// 1
+	if (x == (rows - 1) && y == (columns -1))
+	{
+		xNeighbour = 0;
+		yNeighbour = 0;
+		neighbourIndex = getIndex(x, y, rows);
+	}
+	// 2
+	else if (y == (columns - 1))
+	{
+		xNeighbour = x + 1;
+		yNeighbour = 0;
+		neighbourIndex = getIndex(xNeighbour, yNeighbour, rows);
+	}
+	// 3
+	else if (x == (rows - 1))
+	{
+		neighbourIndex = index + 3;
+	}
+	// 4
+	else
+	{
+		neighbourIndex = getIndex(x + 1, y + 1, rows);
+	}
+
+	if (gen[neighbourIndex + 2] == 1) 
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+__device__
+int getCellNeighbours(int index, int *gen, int rows, int columns){
+	int neighbours = 0;
+	neighbours = checkLeft(index, gen, rows);
+	neighbours = neighbours + checkRight(index, gen, rows);
+	neighbours = neighbours + checkTop(index, gen, rows, columns);
+	neighbours = neighbours + checkBottom(index, gen, rows, columns);
+	neighbours = neighbours + checkDiagonalTL(index, gen, rows, columns);
+	neighbours = neighbours + checkDiagonalTR(index, gen, rows, columns);
+	neighbours = neighbours + checkDiagonalBL(index, gen, rows, columns);
+	neighbours = neighbours + checkDiagonalBR(index, gen, rows, columns);
+	/*
+	printf(" x : %d , y : %d. Check LEft neighbours : %d \n", gen[index], gen[index + 1], neighbours);
+	printf(" x : %d , y : %d. Check right neighbours : %d \n", gen[index], gen[index + 1], checkRight(index, gen, rows));
+	printf(" x : %d , y : %d. Check Top neighbours : %d \n", gen[index], gen[index + 1], checkTop(index, gen, rows, columns));
+	printf(" x : %d , y : %d. Check B neighbours : %d \n", gen[index], gen[index + 1], checkBottom(index, gen, rows, columns));
+	printf(" x : %d , y : %d. Check TL neighbours : %d \n", gen[index], gen[index + 1], checkDiagonalTL(index, gen, rows, columns));
+	printf(" x : %d , y : %d. Check TR neighbours : %d \n", gen[index], gen[index + 1], checkDiagonalTR(index, gen, rows, columns));
+	printf(" x : %d , y : %d. Check BL neighbours : %d \n", gen[index], gen[index + 1], checkDiagonalBL(index, gen, rows, columns));
+	printf(" x : %d , y : %d. Check BR neighbours : %d \n", gen[index], gen[index + 1], checkDiagonalBR(index, gen, rows, columns));
+	*/
+	return neighbours;
+}
+
+__device__
+void cellNextCycle(int *gen, int *newGen, int index, int rows, int columns){
+
+	int neighbours = 0;
+	neighbours = getCellNeighbours(index, gen, rows, columns);
+	
+	printf("\nx : %d , y : %d. neighbours : %d \n", gen[index], gen[index + 1], neighbours);
+
+	// Any live cell
+	if (gen[index + 2]== 1)
+	{
+		//Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+		if (neighbours < 2)
+		{
+			newGen[index + 2] = 0;
+		} //Any live cell with more than three live neighbours dies, as if by overpopulation.
+		else if (neighbours == 2 || neighbours == 3)
+		{
+			newGen[index + 2] = 1;
+		}
+		else if (neighbours > 3)
+		{
+			newGen[index + 2] = 0;
+		}
+	}
+	else
+	{
+		//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+		if (neighbours == 3 && gen[index + 2] == 0)
+		{
+			newGen[index + 2] = 1;
+		}
+	}
+}
+
+// Gets every cells next value which gets stored in newGen
 __global__
+void calculateBoard(int *gen, int *newGen, int amountofCells, int switchh, int rows, int columns)
+{
+	int count = 0;
+
+	if (switchh % 2 == 0){
+		for(int x = 0; x < amountofCells; x++){
+			cellNextCycle(gen, newGen, count, rows, columns);
+			count = count + 3;
+		}
+	}
+	else
+	{
+		for(int x = 0; x < amountofCells; x++){
+			cellNextCycle(newGen, gen, count, rows, columns);
+			count = count + 3;
+		}
+	}
+}
+
+int getIndexCPU(int x, int y, int rows){
+	// (size * 3) * y + (x * 3)
+	int result;
+	result = (rows * 3) * y;
+	result = result + (x * 3);
+	return result;
+}
+
 void print(int *gen, int amount){
 	
 	printf("\nPrinting Current Gen Array !! [x,y,alive] \n");
@@ -20,9 +421,7 @@ void print(int *gen, int amount){
 	printf("]");
 }
 
-// I can do this in parallel right ?
-__global__
-void printBoard(int *gen ,int amountofCells, int size){
+void printBoard(int *gen ,int amountofCells, int rows){
 
 	int count = 0;
 	int rowCount = 0;
@@ -37,7 +436,7 @@ void printBoard(int *gen ,int amountofCells, int size){
 		}
 		rowCount++;
 
-		if(rowCount == size){
+		if(rowCount == rows){
 			printf("\n");
 			rowCount = 0;
 		}
@@ -45,14 +444,35 @@ void printBoard(int *gen ,int amountofCells, int size){
 	}
 }
 
-// I can do this in parallel right ?
-__global__
-void populateArrays(int *gen, int *newGen, int size){
+void setupGlider(int *gen, int *newGen, int rows){
+
+	int index;
+	index = getIndexCPU(1,0,rows) + 2;
+	gen[index] = 1;
+	newGen[index] = 1;
+
+	index = getIndexCPU(2,1,rows) + 2;
+	gen[index] = 1;
+	newGen[index] = 1;
+
+	index = getIndexCPU(0,2,rows) + 2;
+	gen[index] = 1;
+	newGen[index] = 1;
+
+	index = getIndexCPU(1,2,rows) + 2;
+	gen[index] = 1;
+	newGen[index] = 1;
+
+	index = getIndexCPU(2,2,rows) + 2;
+	gen[index] = 1;
+	newGen[index] = 1;
+}
+
+void populateArrays(int *gen, int *newGen, int rows, int columns){
 	
 	int count = 0;
-    for (int y = 0; y < size; y++) {
-		for(int x = 0; x < size; x++){
-
+    for (int y = 0; y < columns; y++) {
+		for(int x = 0; x < rows; x++){
 			gen[count] = x;
 			newGen[count] = x;
 			count++;
@@ -66,111 +486,55 @@ void populateArrays(int *gen, int *newGen, int size){
 	}
 }
 
-__device__
-int getIndex(int x, int y, int size){
-	// (size * 3) * y + (x * 3)
-	int result;
-	result = (size * 3) * y;
-	result = result + (x * 3);
-	return result;
-}
-
-__global__ 
-void setupGlider(int *gen, int size){
-
-	int index;
-	index = getIndex(1,0,size) + 2;
-	gen[index] = 1;
-
-	index = getIndex(2,1,size) + 2;
-	gen[index] = 1;
-
-	index = getIndex(0,2,size) + 2;
-	gen[index] = 1;
-
-	index = getIndex(1,2,size) + 2;
-	gen[index] = 1;
-
-	index = getIndex(2,2,size) + 2;
-	gen[index] = 1;
-}
-
-
-__device__
-int getCellNeighbours(int xPos, int yPos){
-	return 0;
-}
-__device__
-void cellNextCycle(int *gen, int *newGen, int n){
-	int neighbours = 0;
-
-	// Any live cell
-	if (gen[2]== 1)
-	{
-		//Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-		if (neighbours < 2)
-		{
-			//cells[y][x].nextState = 0;
-		} //Any live cell with more than three live neighbours dies, as if by overpopulation.
-		else if (neighbours == 2 || neighbours == 3)
-		{
-			//cells[y][x].nextState = 1;
-		}
-		else if (neighbours > 3)
-		{
-			//cells[y][x].nextState = 0;
-		}
-	}
-	else
-	{
-		//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-		if (neighbours == 3 && gen[3] == 0)
-		{
-			//cells[y][x].nextState = 1;
-		}
-	}
-}
-
-// Gets every cells next value which gets stored in newGen
-__global__
-void calculateBoard(int *gen, int *newGen){
-
-
-
-}
-
 int main(void){
 	
 	int *gen, *newGen;
-	int size = 5;
-	int amountofCells = size * size;
+
+	int rows = 5;
+	int columns = 5;
+	int amountofCells = rows * columns;
 	int lengthofArray = ((amountofCells * 2) + amountofCells);
 	int loopCount = 0;
 	
-	printf("User wants size : %d , Total Cells needed : %d , Array Size : %d \n", size, (size * size), lengthofArray);
+	printf("User wants %d X %d , Total Cells needed : %d , Array Size : %d \n", rows , columns, amountofCells, lengthofArray);
 
 	// Allocate Unified Memory – accessible from CPU or GPU
     cudaMallocManaged(&gen, lengthofArray*sizeof(int));
 	cudaMallocManaged(&newGen, lengthofArray*sizeof(int));
 
 	// populate board
-	populateArrays<<<1,1>>>(gen, newGen, size);
+	populateArrays(gen, newGen, rows, columns);
 
 	// set up glider
-	setupGlider<<<1,1>>>(gen, size);
+	setupGlider(gen, newGen, rows);
+	cudaDeviceSynchronize();
 
 	// Keep calculating board & printing
-	while(loopCount < 1){
-		printBoard<<<1,1>>>(gen, lengthofArray, size);
-		//calculateBoard<<1,amountofCells>>>(gen, newGen);
+	while(loopCount < 3){
+
+		calculateBoard<<<1,1>>>(gen, newGen, amountofCells, loopCount, rows, columns);
+		
+		cudaDeviceSynchronize();
+
+		if (loopCount % 2 == 0){
+			printBoard(gen, lengthofArray, rows);
+			//print(gen, lengthofArray);
+		}
+		else
+		{
+			printBoard(newGen, lengthofArray, rows);
+			//print(newGen, lengthofArray);
+		}
+		
 		loopCount++;
 	}
-	
+
 	// Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();
 	// Free memory
 	cudaFree(gen);
 	cudaFree(newGen);
+
 	return 0;
 }
-
+	
